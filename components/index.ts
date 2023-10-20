3241501f283e90c3254a1a9b487c79bfd314d4a6
JesusC-R <cuevasrodartej@gmail.com>
2023-10-19 21:23:38 -0700

    Fix: Modify components imports.

---
+++ components/index.ts
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CustomButton from "./CustomeButton";
+
+
+export {
+  Hero,
+  Navbar,
+  Footer,
+  CustomButton,
+}
