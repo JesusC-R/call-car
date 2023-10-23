11d637417ebcbae411fa7e878d8b3493eb175b10
JesusC-R <cuevasrodartej@gmail.com>
2023-10-22 18:42:17 -0700

    Chor: Update imports/exports and interfaces.

--- components/index.ts
+++ components/index.ts
@@ -2,6 +2,9 @@ import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import CustomButton from "./CustomeButton";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
 
 export {
@@ -9,4 +12,7 @@ export {
   Navbar,
   Footer,
   CustomButton,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
 }
