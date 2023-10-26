6b6d64324d39941ac6bc3eacf30549640ca6c32b
JesusC-R <cuevasrodartej@gmail.com>
2023-10-25 20:04:49 -0700

    Chor: Add car rent calculation function.

--- utils/index.ts
+++ utils/index.ts
@@ -5,11 +5,26 @@ export async function fetchCars() {
   };
 
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars",
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
     { headers: headers }
   );
 
   const result = await response.json();
 
   return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const milageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on milage and age
+  const milageRate = city_mpg * milageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day;
+  const rentalRatePerDay = basePricePerDay + milageRate + ageRate;
+
+  return rentalRatePerDay;
 }
