b0de35c3a2c39ee3a76fe19fc29959738230556c
JesusC-R <cuevasrodartej@gmail.com>
2023-11-07 23:55:26 -0800

    Fix: Modify page component to be client component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,45 @@
+"use client";
+
 import { Hero, CustomFilter, CarCard, SearchBar, ShowMore } from "@/components";
 import { fuels, yearsOfProduction } from "@/constants";
-import { CarProps } from "@/types";
 import { fetchCars } from "@/utils";
+import { useEffect, useState } from "react";
 
-export default async function Home({ searchParams }: any) {
-  const allCars = await fetchCars({
-    manufacturer: searchParams.manufacturer || "",
-    year: searchParams.year || 2022,
-    fuel: searchParams.fuel || "",
-    limit: searchParams.limit || 10,
-    model: searchParams.model || "",
-  });
+export default function Home() {
+  const [allCars, setAllCars] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  // Search States
+  const [manufacturer, setManufacturer] = useState("");
+  const [model, setModel] = useState("");
+
+  // Filter States
+  const [fuel, setFuel] = useState("");
+  const [year, setYear] = useState(202);
+
+  // Pagination State
+  const [limit, setLimit] = useState(10);
+
+  const getCars = async () => {
+    setLoading(true);
+    try {
+      const result = await fetchCars({
+        manufacturer: manufacturer || "",
+        year: year || 2022,
+        fuel: fuel || "",
+        limit: limit || 10,
+        model: model || "",
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getCars();
+  }, [allCars, loading, manufacturer, model, fuel, year, limit]);
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
@@ -27,7 +56,7 @@ export default async function Home({ searchParams }: any) {
 
           <div className="home__filter-container">
             <CustomFilter title="fuel" options={fuels} />
-            <CustomFilter title="year" options={yearsOfProduction}/>
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
 
